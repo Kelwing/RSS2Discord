ea211be260a1bd05f848fb18883b7561babb526a
Jacob Wiltse <kelwing@kelnet.org>
2017-08-19 01:32:39 -0400

    Corrected the models

--- models.py
+++ models.py
@@ -1,13 +1,16 @@
 from peewee import *
 db = SqliteDatabase('feeds.db')
 
+class BaseModel(Model):
+    class Meta:
+        database = db
 
-class Feed(Model):
+class Feed(BaseModel):
     url = CharField(max_length=500)
     initialized = BooleanField(default=False)
 
 
-class Article(Model):
+class Article(BaseModel):
     id = CharField(max_length=500)
     title = CharField(max_length=300)
     link = CharField(max_length=500)
@@ -15,11 +18,11 @@ class Article(Model):
     feed = ForeignKeyField(Feed)
 
 
-class Destination(Model):
+class Destination(BaseModel):
     url = CharField(max_length=200)
 
 
 # Many-to-many relationship :-)
-class Link(Model):
+class Link(BaseModel):
     feed = ForeignKeyField(Feed)
     webhook = ForeignKeyField(Destination)
